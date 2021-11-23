dc5c9dda35d17062323c35476abe33342301f69c
Caroline Arcos <carospecht@gmail.com>
2021-11-23 14:03:05 +0100

    ...

--- src/main/java/WebServer.java
+++ src/main/java/WebServer.java
@@ -16,6 +16,7 @@ import java.sql.PreparedStatement;
 public class WebServer extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
+
     protected void doPost(HttpServletRequest request,
                           HttpServletResponse response)
             throws ServletException, IOException
