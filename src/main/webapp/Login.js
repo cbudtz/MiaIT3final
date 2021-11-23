1cbd1fa9f0435b5d54561953a948d55e5d2eb3b0
Caroline Arcos <carospecht@gmail.com>
2021-11-23 17:58:22 +0100

    ...16:31

---
+++ src/main/webapp/Login.js
@@ -0,0 +1,17 @@
+function hent() {
+    user = document.getElementById("Brugernavn").value;
+    password = document.getElementById("password").value;
+    fetch("http://localhost:8080/IT3final_war/rest/Login?" + new URLSearchParams({
+        Brugernavn: user,
+        password: password,
+}
+
+    )).then(async response => {
+        if (response.status >=200 && response.status <=299) {
+            return response.text();
+        } else {
+            throw Error (await response.text());
+        }
+        }).then()
+        )
+}
