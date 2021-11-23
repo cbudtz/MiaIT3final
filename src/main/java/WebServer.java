cc7a1406a27d521317495178d89364ece8d1a5a5
Caroline Arcos <carospecht@gmail.com>
2021-11-23 15:14:39 +0100

    ...hhhh

--- src/main/java/WebServer.java
+++ src/main/java/WebServer.java
@@ -17,9 +17,7 @@ public class WebServer extends HttpServlet {
     private static final long serialVersionUID = 1L;
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        PrintWriter writer = response.getWriter();
-        writer.println("Hello!");
-        writer.close();
+                response.sendRedirect("Login.html");
     }
 
     protected void doPost(HttpServletRequest request,
